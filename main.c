fb5ac732e9a44b6a4329713642cc39bf35ebdf08
AlainDlcTVOP <alain_dlc@outlook.com>
2019-11-21 11:25:40 +0100

    created a pointer

--- main.c
+++ main.c
@@ -3,23 +3,32 @@
 #include <stdint.h>
 
 //Define the struct
-struct weekdays {
+typedef struct
+{
     uint8_t number;
-    char* name;
-};
-
+    char *name;
+} Weekdays;
 
 int main()
 {
-	//Declare and initialize a struct to represent week.
-
+    //Declare and initialize a struct to represent week.
+    Weekdays day[] = {{0, "monday"}, {1, "tuesday"}, {2, "wednesday"}, {3, "thursday"}, {4, "friday"}, {5, "saturday"}, {6, "sunday"}};
 
     //Use the array and print the first and last day
-
-
+    printf("%s\n", day[0].name);
+    printf("%s\n", day[6].name);
     //Create a pointer to the struct array
-  
-	//Use the array pointer and print the first and last day
 
+    Weekdays *pday; // Pekare till struts
+    pday = day;
+
+  
+    //Use the array pointer and print the first and last day
+ 
+
+    printf("%s\n",pday[0].name);
+    printf("%s\n",pday[6].name);
+
+    free(pday);
     return 0;
 }
